9e4add5b945c11eed94e369391a4bc62ceed0efc
Ngoundje <ngoundjembaye@gmail.com>
2026-04-15 14:18:25 +0000

    Fix tests Spring Boot avec profil test et H2

--- src/test/java/com/groupeisi/diplomasservice/InsertionServiceApplicationTests.java
+++ src/test/java/com/groupeisi/diplomasservice/InsertionServiceApplicationTests.java
@@ -1,10 +1,11 @@
 package com.groupeisi.diplomasservice;
 
+import com.groupeisi.diplomasservice.InsertionServiceApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(classes = InsertionServiceApplication.class)
 @ActiveProfiles("test")
 class InsertionServiceApplicationTests {
 
